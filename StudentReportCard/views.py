5d55c67bc74da6bdd57ac18db90dd969bf92df63
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2023-07-26 17:42:30 +0500

    Student.html page is done to show data of all students.

--- StudentReportCard/views.py
+++ StudentReportCard/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
+
+def get_students(request):
+    students = Student.objects.all()
+    return render(request, 'students.html', {'data' : students})
