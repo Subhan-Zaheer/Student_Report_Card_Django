e4d03e46bcf883486f5f504beaa6ac947a72302d
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2023-07-26 22:02:45 +0500

    pagination added and search in students feature added.

--- StudentReportCard/views.py
+++ StudentReportCard/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render, redirect
 from .models import *
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 # Create your views here.
 
 def get_students(request):
     students = Student.objects.all()
-    return render(request, 'students.html', {'data' : students})
+    if request.GET.get('search'):
+        search = request.GET.get('search')
+        students = Student.objects.filter(
+            Q(student_name__icontains = search) |
+            Q(department__department_name__icontains = search) |
+            Q(student_id__student_id__icontains = search) |
+            Q(student_email__icontains = search),
+        )
+    p = Paginator(students, 5)
+    page_number = request.GET.get('page')
+    final_students_data = p.get_page(page_number)
+    total_pages = p.num_pages
+    data = {
+        'final_data' : final_students_data,
+        'last_page' : total_pages, 
+        'page_list' : [i+1 for i in range(total_pages)]
+    }
+    return render(request, 'students.html', data)
